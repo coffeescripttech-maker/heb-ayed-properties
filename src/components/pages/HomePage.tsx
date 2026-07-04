035a30cec3874a9ebadbbb33c790f80e041a39c0
xedter20 <dextermiranda441@gmail.com>
2026-07-04 16:15:35 +0800

    comment

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,16 +1,16 @@
 import { HeroSection } from '../sections/HeroSection';
-import { DealsSection } from '../sections/DealsSection';
-import { HowIChooseSection } from '../sections/HowIChooseSection';
-import { StorySection } from '../sections/StorySection';
-import { AwardsSection } from '../sections/AwardsSection';
-import { CeremoniesSection } from '../sections/CeremoniesSection';
-import { WhyDubaiSection } from '../sections/WhyDubaiSection';
-import { TestimonialsSection } from '../sections/TestimonialsSection';
-import { StepsSection } from '../sections/StepsSection';
-import { InquirySection } from '../sections/InquirySection';
-import { OfficeSection } from '../sections/OfficeSection';
-import { NewsSection } from '../sections/NewsSection';
-import { QuotesSection } from '../sections/QuotesSection';
+// import { DealsSection } from '../sections/DealsSection';
+// import { HowIChooseSection } from '../sections/HowIChooseSection';
+// import { StorySection } from '../sections/StorySection';
+// import { AwardsSection } from '../sections/AwardsSection';
+// import { CeremoniesSection } from '../sections/CeremoniesSection';
+// import { WhyDubaiSection } from '../sections/WhyDubaiSection';
+// import { TestimonialsSection } from '../sections/TestimonialsSection';
+// import { StepsSection } from '../sections/StepsSection';
+// import { InquirySection } from '../sections/InquirySection';
+// import { OfficeSection } from '../sections/OfficeSection';
+// import { NewsSection } from '../sections/NewsSection';
+// import { QuotesSection } from '../sections/QuotesSection';
 
 export function HomePage() {
   return (
